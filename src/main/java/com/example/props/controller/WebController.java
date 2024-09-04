e402f8d9f92332775bf00f2e31293133d6413dd4
Java Dev <dev@1ad.in>
2024-09-04 23:48:03 +0530

    Autowired ManualClass

--- src/main/java/com/example/props/controller/WebController.java
+++ src/main/java/com/example/props/controller/WebController.java
@@ -1,5 +1,6 @@
 package com.example.props.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,14 +8,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebController {
 
+    @Autowired
+    private ManualClass manualClass;
+
     @Value("${props.name}")
     private String propName;
 
     @GetMapping("/")
     public String showOutput () {
         System.out.println(propName);
-
-        ManualClass manualClass = new ManualClass();
         manualClass.printPropName();
         return "Hello World!";
     }
